1109562b6fdc1b18248aa2a578702eddcf7a5600
itayfry <itayfry@gmail.com>
2023-12-03 21:45:17 +0200

    day 03

--- challenges/03/solution/part1.js
+++ challenges/03/solution/part1.js
@@ -1,5 +1,73 @@
 const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
+let currentNumberStr = '';
+let sum = 0;
 
-console.log(res)
+const hasAdjecentSymbol = (row, startColumn, endColumn) => {
+    const checkStart = Math.max(startColumn - 1, 0)
+    const checkEnd = Math.min(endColumn + 1, arr[0].length - 1)
+    if (row > 0) {
+        for (let col = checkStart; col <= checkEnd; col++) {
+            if (isSymbol(arr[row - 1][col])) {
+                return true;
+            }
+        }
+    }
+    if (row < arr.length - 1) {
+        for (let col = checkStart; col <= checkEnd; col++) {
+            if (isSymbol(arr[row + 1][col])) {
+                return true;
+            }
+        }
+    }
+    if (startColumn > 0) {
+        if (isSymbol(arr[row][startColumn - 1])) {
+            return true;
+        }
+    }
+    if (endColumn + 1 < arr[row].length) {
+        if (isSymbol(arr[row][endColumn + 1])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const isSymbol = char => char !== undefined && char !== '.' && Number.isNaN(Number(parseInt(char)))
+
+let nums = []
+let validNums = []
+let invalidNums = []
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        const char = arr[i][j]
+        if (!Number.isNaN(Number(parseInt(char)))) {
+            currentNumberStr += char
+            if (j === arr[i].length - 1) {
+                nums.push(currentNumberStr)
+                if (hasAdjecentSymbol(i, j - currentNumberStr.length + 1, j)) {
+                    validNums.push(currentNumberStr)
+                    sum += Number(currentNumberStr)
+                }
+                else {
+                    invalidNums.push(currentNumberStr)
+                }
+                currentNumberStr = ''
+            }
+        }
+        else if (currentNumberStr) {
+            nums.push(currentNumberStr)
+            if (hasAdjecentSymbol(i, j - currentNumberStr.length, j - 1)) {
+                validNums.push(currentNumberStr)
+                sum += Number(currentNumberStr)
+            }
+            else {
+                invalidNums.push(currentNumberStr)
+            }
+            currentNumberStr = ''
+        }
+    }
+}
+
+console.log(sum)
--- challenges/03/solution/part2.js
+++ challenges/03/solution/part2.js
@@ -2,4 +2,65 @@ const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
 
+const starLocations = []
+
+for (let row = 0; row < arr.length; row++) {
+    for (let col = 0; col < arr[0].length; col++) {
+        if (arr[row][col] === '*') {
+            starLocations.push([row, col])
+        }
+    }
+}
+
+let nums = []
+let currentNumberStr = ''
+let id = 1
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        const char = arr[i][j]
+        if (!Number.isNaN(Number(parseInt(char)))) {
+            currentNumberStr += char
+            if (j === arr[i].length - 1) {
+                nums[id++] = { val: Number(currentNumberStr), row: i, startCol: j - currentNumberStr.length + 1, endCol: j }
+                currentNumberStr = ''
+            }
+        }
+        else if (currentNumberStr) {
+            nums[id++] = { val: Number(currentNumberStr), row: i, startCol: j - currentNumberStr.length, endCol: j - 1 }
+            currentNumberStr = ''
+        }
+    }
+}
+
+const numIdsMap = {}
+Object.entries(nums).forEach(([id, numObj]) => {
+    if (!numIdsMap[numObj.row]) {
+        numIdsMap[numObj.row] = {}
+    }
+    for (let i = numObj.startCol; i <= numObj.endCol; i++) {
+        numIdsMap[numObj.row][i] = id
+    }
+})
+let res = 0;
+starLocations.forEach(([row, col]) => {
+    const neighbors = [
+        [row - 1, col - 1],
+        [row - 1, col],
+        [row - 1, col + 1],
+        [row, col - 1],
+        [row, col + 1],
+        [row + 1, col - 1],
+        [row + 1, col],
+        [row + 1, col + 1],
+    ]
+    neighborsValues = new Set()
+    neighbors.map(([r, c]) => numIdsMap[r] && numIdsMap[r][c]).filter(x => x).forEach(id => {
+        neighborsValues.add(id)
+    })
+    const nValsArr = Array.from(neighborsValues)
+    if (nValsArr.length === 2) {
+        res += nums[nValsArr[0]].val * nums[nValsArr[1]].val
+    }
+})
+
 console.log(res)
