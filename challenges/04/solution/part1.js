59d26ea74079a38a8d1919bb88390b07bff1cbc1
itayfry <itayfry@gmail.com>
2023-12-04 18:40:22 +0200

    day 04

--- challenges/04/solution/part1.js
+++ challenges/04/solution/part1.js
@@ -2,4 +2,14 @@ const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
 
+let res = 0;
+for (const line of arr) {
+    const [winningNums, cardNums] = line.split(': ')[1].split(' | ').map( x => x.trim().split(/[' ']+/))
+    const matching = cardNums.filter( x => winningNums.indexOf(x) !== -1)
+    const count = matching.length
+    if (count){
+        res += Math.pow(2, count -1)
+    }
+}
+
 console.log(res)
--- challenges/04/solution/part2.js
+++ challenges/04/solution/part2.js
@@ -2,4 +2,20 @@ const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
 
-console.log(res)
+const cardsData = arr.map(line => {
+    const [winningNums, cardNums] = line.split(': ')[1].split(' | ').map(x => x.trim().split(/[' ']+/))
+    const matching = cardNums.filter(x => winningNums.indexOf(x) !== -1)
+    const count = matching.length
+    return { instances: 1, wins: count }
+})
+
+for (let i = 0; i < cardsData.length; i++){
+    const {wins} = cardsData[i]
+    if(wins){
+        for(let j = i + 1; j <= i + wins; j++ ){
+            cardsData[j].instances+= cardsData[i].instances
+        }
+    }
+}
+
+console.log(cardsData.reduce((a, obj) => a + obj.instances, 0))
