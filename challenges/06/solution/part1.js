69a000a3e741ee709f77db118e58a308e286ca7f
itayfry <itayfry@gmail.com>
2023-12-06 20:32:14 +0200

    day 06

--- challenges/06/solution/part1.js
+++ challenges/06/solution/part1.js
@@ -1,5 +1,18 @@
 const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
+const times = arr[0].split(/Time:\s*/)[1].split(/\s+/).map(Number)
+const distances = arr[1].split(/Distance:\s*/)[1].split(/\s+/).map(Number)
+const data = times.map((time, index) => ({ time, distance: distances[index] }))
+const optionsCount = data.map(({ time, distance }) => {
+    let right = Math.ceil(time / 2)
+    let left = time - right
+    let count = 0
+    while (right++ * left-- > distance && left > 0) {
+        count += 2
+    }
+    return time % 2 ? count : count - 1
+})
 
+const res = optionsCount.reduce((a, b) => a * b, 1)
 console.log(res)
--- challenges/06/solution/part2.js
+++ challenges/06/solution/part2.js
@@ -1,5 +1,13 @@
 const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
+const time = Number(arr[0].split(/Time:\s*/)[1].split(/\s+/).join(''))
+const distance = Number(arr[1].split(/Distance:\s*/)[1].split(/\s+/).join(''))
 
-console.log(res)
+let right = Math.ceil(time / 2)
+let left = time - right
+let count = 0
+while (right++ * left-- > distance && left > 0) {
+    count += 2
+}
+console.log(time % 2 ? count : count - 1)
