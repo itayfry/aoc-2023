2411f33f2e2667751b9bad075c3bca6ccbd4cf53
itayfry <itayfry@gmail.com>
2023-12-08 08:37:43 +0200

    day 08

--- challenges/08/solution/part1.js
+++ challenges/08/solution/part1.js
@@ -2,4 +2,24 @@ const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
 
-console.log(res)
+const directions = arr[0]
+
+arr.shift()
+arr.shift()
+
+const graph = arr.reduce((acc, line) => {
+    const [_, curr, L, R] = line.match(/([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)/)
+    acc[curr] = { L, R }
+    return acc
+}, {})
+
+let curr = 'AAA'
+let stepsCount = 0;
+while (curr !== 'ZZZ') {
+    for (const step of directions) {
+        stepsCount++
+        curr = graph[curr][step]
+    }
+}
+
+console.log(stepsCount)
--- challenges/08/solution/part2.js
+++ challenges/08/solution/part2.js
@@ -2,4 +2,29 @@ const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
 
-console.log(res)
+const directions = arr[0]
+
+arr.shift()
+arr.shift()
+
+const graph = arr.reduce((acc, line) => {
+    const [_, curr, L, R] = line.match(/([0-9A-Z]+) = \(([0-9A-Z]+), ([0-9A-Z]+)\)/)
+    acc[curr] = { L, R }
+    return acc
+}, {})
+
+const currPositions = Object.keys(graph).filter(key => key.endsWith('A'))
+const cycles = currPositions.map(curr => {
+    let cyclesCount = 0;
+    while (!curr.endsWith('Z')) {
+        cyclesCount++
+        for (const step of directions) {
+            curr = graph[curr][step]
+        }
+    }
+    return cyclesCount
+})
+
+const mul = cycles.reduce((a, b) => a * b, 1)
+
+console.log(mul * directions.length) // since all cycles are prime numbers. If they weren't I would divide by common denominator
