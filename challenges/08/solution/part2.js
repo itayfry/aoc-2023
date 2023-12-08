8d978d6b50cfcf9fe86923a18a0276a3e4391551
itayfry <itayfry@gmail.com>
2023-12-08 08:48:54 +0200

    day 08 - optional addition

--- challenges/08/solution/part2.js
+++ challenges/08/solution/part2.js
@@ -14,7 +14,7 @@ const graph = arr.reduce((acc, line) => {
 }, {})
 
 const currPositions = Object.keys(graph).filter(key => key.endsWith('A'))
-const cycles = currPositions.map(curr => {
+let cycles = currPositions.map(curr => {
     let cyclesCount = 0;
     while (!curr.endsWith('Z')) {
         cyclesCount++
@@ -25,6 +25,19 @@ const cycles = currPositions.map(curr => {
     return cyclesCount
 })
 
+// All cycles in my input are prime numbers so this is redundant. But if they did have a common denominator this would be required
+const maxCommonDenominator = Math.min(...cycles.map(x => Math.floor(Math.sqrt(x))))
+let i = 2;
+while (i <= maxCommonDenominator) {
+    if (cycles.every(x => !(x % i))) {
+        cycles = cycles.map(x => x / i)
+        i = 2
+    }
+    else {
+        i++
+    }
+}
+
 const mul = cycles.reduce((a, b) => a * b, 1)
 
-console.log(mul * directions.length) // since all cycles are prime numbers. If they weren't I would divide by common denominator
+console.log(mul * directions.length) 
