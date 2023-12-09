27fa6193b7ace96166318b80e6d6734e815fe5cc
itayfry <itayfry@gmail.com>
2023-12-09 09:13:35 +0200

    day 09

--- challenges/09/solution/part1.js
+++ challenges/09/solution/part1.js
@@ -2,4 +2,25 @@ const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
 
+const res = arr.map(line => {
+    let nums = line.split(' ').map(Number)
+    const queue = [nums];
+    while (! queue[queue.length-1].every(x => x === 0)){
+        const nextNums = []
+        for (let i = 0; i < nums.length - 1; i++){
+            nextNums[i] = nums[i+1] - nums[i]
+        }
+        nums = nextNums
+        queue.push(nextNums)
+    }
+    let increaseBy = 0;
+    for(let i = queue.length - 2; i >= 0; i--){
+        const currentLine = queue[i]
+        currentLine.push(currentLine[currentLine.length - 1] + increaseBy)
+        increaseBy = currentLine[currentLine.length - 1]
+    }
+    return increaseBy;
+    
+}).reduce((a,b) => a + b, 0)
+
 console.log(res)
--- challenges/09/solution/part2.js
+++ challenges/09/solution/part2.js
@@ -2,4 +2,25 @@ const { getInputArrForSolutionDir } = require("../../../utils");
 
 const arr = getInputArrForSolutionDir(__dirname);
 
+const res = arr.map(line => {
+    let nums = line.split(' ').map(Number)
+    const queue = [nums];
+    while (! queue[queue.length-1].every(x => x === 0)){
+        const nextNums = []
+        for (let i = 0; i < nums.length - 1; i++){
+            nextNums[i] = nums[i+1] - nums[i]
+        }
+        nums = nextNums
+        queue.push(nextNums)
+    }
+    let decreaseBy = 0;
+    for(let i = queue.length - 2; i >= 0; i--){
+        const currentLine = queue[i]
+        currentLine.unshift(currentLine[0] - decreaseBy)
+        decreaseBy = currentLine[0]
+    }
+    return decreaseBy;
+    
+}).reduce((a,b) => a + b, 0)
+
 console.log(res)
